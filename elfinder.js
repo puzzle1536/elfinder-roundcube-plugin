8fe917949fd38a21aecb6fbfbd3b434ed59f0f47
Puzzle <puzzle1536@gmail.com>
2014-02-15 10:19:25 +0100

    update elfinder jscript

--- elfinder.js
+++ elfinder.js
@@ -1,49 +1,78 @@
+var dialog;
+var displayed;
+
 if (window.rcmail) {
-    briefcase_load = function(elfinder_function) {
-        var fm = $('#elfinder').elfinder({
-            url : 'plugins/elfinder/php/connector.php',
-            lang : 'en',
-            width : 800,
-            rememberLastDir: true,
-//            onlyMimes: ['text'],
-            getFileCallback : function(files, fm) {
+    $(document).ready(function() {
+        $( "#mainscreen" ).append( "<div id=elfinder class=popupmenu></div>" );
+    })
 
-                ts = new Date().getTime()
-                rcmail.http_request(elfinder_function,
-                                    { _id:rcmail.env.compose_id,
-                                      _uploadid:ts, 
-                                      _filepath:files
-                                    });
+    var briefcase_load = function(elfinder_function) {
+       if (!dialog) {
+           console.log('dialog create');
 
-            },
-            commandsOptions : {
-                getfile : {
-                    oncomplete : 'destroy',
-                    folders : false
+           dialog = $('#elfinder').elfinder({
+                   url : 'plugins/elfinder/php/connector.php',
+                   lang : 'en',
+                   width : 800,
+                   commandsOptions : {
+                       getfile : {
+                           folders : false,
+                       }
+                   },
+                   getFileCallback : function(files, fm) {
+                    
+                       ts = new Date().getTime()
+                       rcmail.http_request(elfinder_function,
+                                           { _id:rcmail.env.compose_id,
+                                             _uploadid:ts,
+                                             _filepath:files,
+                                           });
                 }
-            }
-        });
+            });
+            displayed = true;
+        } else {
+           if (!displayed) {
+               dialog.elfinder('show');
+               displayed = true;
+           } else {
+               dialog.elfinder('hide');
+               displayed = false;
+           }
+        }
     }
 
-    briefcase_save = function(elfinder_function) {
-        var fm = $('#elfinder').elfinder({
-            url : 'plugins/elfinder/php/connector.php',
-            lang : 'en',
-            width : 800,
-            rememberLastDir: true,
-//            onlyMimes: ['directory'],
-            getFileCallback : function(folder, fm) {
-                rcmail.http_request(elfinder_function,
-                                    { _dirpath:folder,
-                                      _mbox:rcmail.env.mailbox,
-                                      _uid:rcmail.env.uid });
-            },
-            commandsOptions : {
-                getfile : {
-                    oncomplete : 'destroy',
-                    folders : true
+    var briefcase_save = function(elfinder_function) {
+       if (!dialog) {
+           console.log('dialog create');
+
+           dialog = $('#elfinder').elfinder({
+                   url : 'plugins/elfinder/php/connector.php',
+                   lang : 'en',
+                   width : 800,
+                   commandsOptions : {
+                       getfile : {
+                           folders : true,
+                       }
+                   },
+                   getFileCallback : function(folder, fm) {
+                    
+                    rcmail.http_request(elfinder_function,
+                                        { _dirpath:folder,
+                                          _mbox:rcmail.env.mailbox,
+                                          _uid:rcmail.env.uid });
+    
                 }
-            }
-        });
+            });
+            displayed = true;
+        } else {
+           if (!displayed) {
+               dialog.elfinder('show');
+               displayed = true;
+           } else {
+               dialog.elfinder('hide');
+               displayed = false;
+           }
+        }
     }
+
 }
